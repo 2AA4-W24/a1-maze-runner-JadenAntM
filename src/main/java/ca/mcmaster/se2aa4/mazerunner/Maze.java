13b00640e9c5ca83d959c623403cac0959ac2725
root <root@Jaden>
2024-01-23 21:23:24 -0500

    Added class to read maze and store in 2d array

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,10 +1,36 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Maze {
-    // representation of maze
+    private char[][] mazeArray; // 2D array
 
     public Maze(String inputFile) {
-        // read maze
+        readMazeFromFile(inputFile);
+    }
+
+    private void readMazeFromFile(String inputFile) {
+        // List to store the maze
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+            // read the lines of maze and store in list
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            // Starts maze array based on size
+            mazeArray = new char[lines.size()][];
+            for (int i = 0; i < lines.size(); i++) {
+                // Stores elements as a row
+                mazeArray[i] = lines.get(i).toCharArray();
+            }
+        } catch (IOException e) {
+            logger.error("error reading file", e);
+        }
     }
 
     public Point findEntryPoint() {
